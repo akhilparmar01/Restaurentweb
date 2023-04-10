8b7906396b7ca9502e98cfcfce9efe6232bcccf9
Akhil  parmar <parmarakhil34@gmail.com>
2023-04-10 06:38:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -10,24 +10,46 @@ import ReactDOM from "react-dom/client";
 </ul>
 </div> */
 
-const title =(
-    <h1 id="title" key="h2">
-Namstey Hero
-    </h1>
-);
+// const title =(
+//     <h1 id="title" key="h2">
+// Namstey Hero
+//     </h1>
+// );
 
 
 // cpmposing component
-const HeaderComponent  =  () => {
-    return(
-         <div>
+// const HeaderComponent  =  () => {
+//     return(
+//          <div>
          
-     {title}
-         <h1>Nmaste React function componrent</h1>
-    <h2>How are you my friend</h2>
-    </div>
-    );
-};
+//      {title}
+//          <h1>Nmaste React function componrent</h1>
+//     <h2>How are you my friend</h2>
+//     </div>
+//     );
+// };
+
+const Text =(
+    <inpute type="text" placeholder="search.."  key="h3">
+Namstey Hero
+    </inpute>
+);
+
+
+
+const HeaderComponent  =  () => {
+        return(
+            
+            <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            
+          </div>
+        );
+    };
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
